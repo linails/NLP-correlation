64d0cbc74456c41aaa92e1432d3474475fb31c1b
minphone <linails@foxmail.com>
2017-03-07 22:34:18 +0800

    move program-train:case002  here

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # NLP-correlation
 NLP - correlation 
+
+------
+#####转载与使用请注明出处 minphone linails@foxmail.com
+------
+
+###[处理涉及内容及使用到的工具等]
+
+> * 使用 C/C++
+> * 使用 python
+> * 词典数据库制作
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 /*
  * Progarm Name: main.cpp
  * Created Time: 2017-03-07 21:53:35
- * Last modified: 2017-03-07 21:53:59
+ * Last modified: 2017-03-07 22:31:13
  * @author: minphone.linails linails@foxmail.com 
  */
 
 #include <iostream>
+#include <cassert>
+#include "dic-parser.hpp"
 
 using std::cout;
 using std::endl;
@@ -15,6 +17,10 @@ int main(int argc, char **argv)
     int ret = 0;
 
     cout << "NLP-correlation ..." << endl;
+
+    DicParser dicparser;
+
+    ret = dicparser.dicparser_main(argc, argv); assert(-1 != ret);
 
     return ret;
 }
