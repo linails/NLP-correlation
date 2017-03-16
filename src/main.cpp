024492926eb675181f2fd4c42ab03b93e7456617
minphone <linails@foxmail.com>
2017-03-16 16:10:56 +0800

    update ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,41 @@
 /*
  * Progarm Name: main.cpp
  * Created Time: 2017-03-07 21:53:35
- * Last modified: 2017-03-14 14:17:00
+ * Last modified: 2017-03-16 09:30:45
  * @author: minphone.linails linails@foxmail.com 
  */
 
 #include <iostream>
 #include <cassert>
 #include "dic-parser.hpp"
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::string;
 
 int main(int argc, char **argv)
 {
     int ret = 0;
 
+    string path;
+
     cout << "NLP-correlation ..." << endl;
 
-    DicParser *pdic = DicParser::get_instance(new DicParser());
+    switch(argc){
+        case 1:
+            break;
+        case 2:
+            path = argv[1];
+            break;
+        case 3:
+            cout << "[Warning] argc = 3" << endl;
+            break;
+    }
+
+    DicParser *pdic = DicParser::get_instance(new DicParser(path));
     if(nullptr != pdic){
-        cout << "new succeed " << endl;
+        cout << "new successed " << endl;
     }else{
         cout << "[Error] new failed !" << endl;
     }
