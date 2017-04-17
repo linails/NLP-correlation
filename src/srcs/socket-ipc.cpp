9484b5b412ed5d058bb55d9ee03b7ec7918ba32a
minphone <linails@foxmail.com>
2017-04-17 17:26:40 +0800

    update cfg-loader / socket-ipc

--- src/srcs/socket-ipc.cpp
+++ src/srcs/socket-ipc.cpp
@@ -1,7 +1,7 @@
 /*
  * Progarm Name: socket-ipc.cpp
  * Created Time: 2017-03-09 08:54:05
- * Last modified: 2017-03-09 08:54:40
+ * Last modified: 2017-04-17 17:25:40
  * @author: minphone.linails linails@foxmail.com 
  */
 
@@ -13,100 +13,239 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include "cfg-loader.hpp"
+#include "dic-parser.hpp"
 
 using std::cout;
 using std::endl;
 
-int server_tcp_echo(int argc, char **argv)
+SocketIPC::SocketIPC()
 {
-    printf("------ server-tcp-echo ------\n");
+    this->register_xml();
 
-    int serv_sock;
-    int client_sock;
+    this->init_socket();
 
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in client_addr;
-
-    socklen_t clnt_addr_size;
-
-    char message[512] = "Hello World - echo!";
-
-
-
-    printf("\n");
-    printf("Print argv[] List :\n{\n");
-    for(int i=0; i<argc; i++){
-        printf("    argv[%d] : %s\n", i, argv[i]);
-    }
-    printf("}\n\n");
-
-
-
-    if(2 != argc){
-        printf("Usage : %s <port> \n", argv[0]);
+    this->m_listener = new thread(&SocketIPC::listener, this);
+    if(nullptr == this->m_listener){
+        cout << "[Error] new failed in ServerTcp - m_listener !" << endl;
         exit(1);
     }
+}
 
-    /******************************************************************************/
+SocketIPC::~SocketIPC()
+{
+    this->m_listener->join();
+    delete this->m_listener;
+    this->m_listener= nullptr;
+
+    if(nullptr != this->m_reader){
+        this->m_reader->join();
+        delete this->m_reader;
+        this->m_reader  = nullptr;
+    }
+
+    if(nullptr != this->m_writer){
+        this->m_writer->join();
+        delete this->m_writer;
+        this->m_writer  = nullptr;
+    }
+
+    /*
+     * close socket
+     * */
+    {
+        if(-1 != this->m_serv_sock){
+            close(this->m_serv_sock); this->m_serv_sock = -1;
+        }
+    }
+}
+
+int  SocketIPC::swrite(string &s)
+{
+    std::unique_lock<std::recursive_mutex> lock(this->m_lock_write);
+    this->m_writer_buf.push_back(s);
+    return 0;
+}
+
+int  SocketIPC::sread(string &s)
+{
+    int ret = 0;
+
+    std::unique_lock<std::recursive_mutex> lock(this->m_lock_read);
+    if(false == this->m_reader_buf.empty()){
+        s = this->m_reader_buf.front();
+        this->m_reader_buf.pop_front();
+
+        ret = s.size();
+    }
+
+    return ret;
+}
+
+int  SocketIPC::register_xml(void)
+{
+    int ret = 0;
+
+    DicParser *pdic = DicParser::get_instance();
+    if(nullptr != pdic){
+
+        CfgLoader *cfg = pdic->get_xml_cfg();
+        if(nullptr != cfg){
+            cfg->register_RootChild("network", "server_port");
+
+            string port;
+            cfg->getRoot_child(port, "network", "server_port");
+            this->m_port = atoi(port.c_str());
+
+            cout << "port = " << this->m_port << endl;
+        }else
+            ret = -1;
+
+    }else
+        ret = -1;
+
+    return ret;
+}
+
+int  SocketIPC::init_socket(void)
+{
+    int ret = 0;
 
     /* Step 1 */
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if(-1 == serv_sock){
-        printf("socket() error !!!\n");
+    this->m_serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(-1 == this->m_serv_sock){
+        cout << "socket() error !!!" << endl;
+        ret = -1;
         exit(1);
     }
 
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family        = AF_INET;
-    serv_addr.sin_addr.s_addr   = htonl(INADDR_ANY);
-    serv_addr.sin_port          = htons(atoi(argv[1]));
+    memset(&this->m_serv_addr, 0, sizeof(this->m_serv_addr));
+    this->m_serv_addr.sin_family        = AF_INET;
+    this->m_serv_addr.sin_addr.s_addr   = htonl(INADDR_ANY);
+    this->m_serv_addr.sin_port          = htons(this->m_port);
 
     /* Step 2 */
-    if(-1 == bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))){
-        printf("bind() error !!!\n");
+    if(-1 == bind(this->m_serv_sock, (struct sockaddr*)&this->m_serv_addr, sizeof(this->m_serv_addr))){
+        cout << "bind() error !!!" << endl;
+        ret = -1;
         exit(1);
     }
 
-    /* Step 3 */
-    if(-1 == listen(serv_sock, 5)){
-        printf("listen() error !!!\n");
-        exit(1);
-    }
+    return ret;
+}
 
-    for(int i=0; i<6; i++){
-        printf("for i = %d\n", i);
-        /* Step 4 */
-        clnt_addr_size  = sizeof(client_addr);
-        client_sock     = accept(serv_sock, (struct sockaddr*)&client_addr, &clnt_addr_size);
-        if(-1 == client_sock){
-            printf("accept() error\n");
-            exit(1);
+int  SocketIPC::reader(void)
+{
+    while(1){
+        if(-1 != this->m_killed) break;
+
+        char buf[1024] = {0, };
+        int  str_len   = read(this->m_client_sock, buf, sizeof(buf));
+
+        cout << "str_len = " << str_len << endl;
+        if(0 != str_len){
+            buf[str_len] = '\0';
+            string s = buf;
+
+            std::unique_lock<std::recursive_mutex> lock(this->m_lock_read);
+            this->m_reader_buf.push_back(s);
+            cout << "reader s = " << s << endl;
         }
 
-        /******************************************************************************/
-
-        printf("Client : %d -> port : %#x\n",
-                client_sock, 
-                client_addr.sin_port);
-
-        int str_len = 0;
-        while(0 != (str_len = read(client_sock, message, sizeof(message)))){
-            write(client_sock, message, str_len);
-            message[str_len] = '\0';
-
-            printf("Message To client : %s -> client : %d -> port : %#x\n",
-                    message, 
-                    client_sock, 
-                    client_addr.sin_port);
-        }
-
-        /* Close all fd */
-        close(client_sock);
+        usleep(100 * 1000);
     }
 
-    close(serv_sock);
+    shutdown(this->m_client_sock, SHUT_RD);
 
     return 0;
+}
+
+int  SocketIPC::writer(void)
+{
+    while(1){
+        if(-1 != this->m_killed) break;
+
+        if(false == this->m_writer_buf.empty()){
+            std::unique_lock<std::recursive_mutex> lock(this->m_lock_write);
+
+            string s = this->m_writer_buf.front();
+            this->m_writer_buf.pop_front();
+
+            write(this->m_client_sock, s.c_str(), s.size());
+        }
+
+        usleep(100 * 1000);
+    }
+
+    shutdown(this->m_client_sock, SHUT_WR);
+
+    return 0;
+}
+
+int  SocketIPC::listener(void)
+{
+    int ret = 0;
+
+    while(1){
+        if(-1 != this->m_killed) break;
+
+        usleep(500 * 1000);
+
+        if(-1 != this->m_serv_sock){
+
+            socklen_t clnt_addr_size;
+
+            /* Step 3 */
+            if(-1 == listen(this->m_serv_sock, 2)){
+                cout << "listen() error !!!" << endl;
+                ret = -1;
+                exit(1);
+            }
+
+            /* Step 4 */
+            clnt_addr_size          = sizeof(this->m_client_addr);
+            this->m_client_sock     = accept(this->m_serv_sock,
+                                             (struct sockaddr*)&this->m_client_addr,
+                                             &clnt_addr_size);
+            if(-1 == this->m_client_sock){
+                cout << "accept() error !!!" << endl;
+                ret = -1;
+                exit(1);
+            }else{
+
+                printf("Client : %d -> port : %#x\n",
+                        this->m_client_sock, 
+                        this->m_client_addr.sin_port);
+
+
+                this->m_reader = new thread(&SocketIPC::reader, this);
+                if(nullptr == this->m_reader){
+                    cout << "[Error] new failed in ServerTcp - m_reader !" << endl;
+                    close(this->m_client_sock);
+                    ret = -1;
+                    exit(1);
+                }
+
+                this->m_writer = new thread(&SocketIPC::writer, this);
+                if(nullptr == this->m_writer){
+                    cout << "[Error] new failed in ServerTcp - m_writer !" << endl;
+                    close(this->m_client_sock);
+                    ret = -1;
+                    exit(1);
+                }
+
+            }
+
+            break;
+        }
+    }
+
+    return ret;
+}
+
+int  SocketIPC::kill_thread(void)
+{
+    this->m_killed = 0; return 0;
 }
 
 
--- src/srcs/socket-ipc.hpp
+++ src/srcs/socket-ipc.hpp
@@ -1,13 +1,55 @@
 /*
  * Progarm Name: socket-ipc.hpp
  * Created Time: 2017-03-09 08:54:09
- * Last modified: 2017-03-09 08:54:10
+ * Last modified: 2017-04-17 17:19:40
  * @author: minphone.linails linails@foxmail.com 
  */
 
 #ifndef _SOCKET_IPC_HPP_
 #define _SOCKET_IPC_HPP_
 
+#include <string>
+#include <thread>
+#include <time.h>
+#include <list>
+#include <mutex>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+
+using std::string;
+using std::thread;
+using std::list;
+using std::recursive_mutex;
+
+class SocketIPC{
+public:
+    SocketIPC();
+    ~SocketIPC();
+    int  swrite(string &s);
+    int  sread(string &s);
+    int  kill_thread(void);
+private:
+    int  register_xml(void);
+    int  init_socket(void);
+    int  reader(void);
+    int  writer(void);
+    int  listener(void);
+private:
+    thread            *m_reader      = nullptr;
+    thread            *m_writer      = nullptr;
+    thread            *m_listener    = nullptr;
+    int                m_killed      = -1;
+    list<string>       m_reader_buf;
+    list<string>       m_writer_buf;
+    struct sockaddr_in m_serv_addr;
+    struct sockaddr_in m_client_addr;
+    int                m_serv_sock   = -1;
+    int                m_client_sock = -1;
+    int                m_port        = -1;
+    recursive_mutex    m_lock_read;
+    recursive_mutex    m_lock_write;
+};
 
 #endif //_SOCKET_IPC_HPP_
 
